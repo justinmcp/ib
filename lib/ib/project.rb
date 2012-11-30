8b86220c0a1460fbcce71644aa5478ec9a9240d2
Justin McPherson <justin@threewaters.net>
2012-11-30 21:42:15 +1000

    Fix for xcodeproj 0.4.0

--- lib/ib/project.rb
+++ lib/ib/project.rb
@@ -1,35 +1,29 @@
 class IB::Project
   def write app_path = "app", resources_path = "resources", pods_headers_path = "vendor/Pods/Headers"
     project = Xcodeproj::Project.new
-    target = project.targets.new_static_library(:ios, 'ui')
+    target = project.new_target(:static_library, 'ui', :ios)
 
-    resources = project.groups.new('path' => resources_path, 'name' => 'Resources')
-    support   = project.groups.new('name' => 'Supporting Files', 'path' => 'ib.xcodeproj')
-    pods      = project.groups.new('name' => 'Pods')
+    resources = project.new_group('Resources')
+    support   = project.new_group('Supporting Files') 
+    pods      = project.new_group('Pods')
 
     IB::Generator.new.write(app_path, "ib.xcodeproj")
-    support.files.new 'path' => 'Stubs.h', 'sourceTree' => '<group>'
 
-    path = Pathname.new("Stubs.m")
-    desc = Xcodeproj::Project::PBXNativeTarget::SourceFileDescription.new(path, nil, nil)
-    file = target.add_source_files([desc]).first
-    file.source_tree = '<group>'
-    support.files << file
+    support.new_file "ib.xcodeproj/Stubs.h"
+    file = support.new_file "ib.xcodeproj/Stubs.m"
+    target.add_file_references([ file ])
 
     resource_exts = %W{xcdatamodeld png jpg jpeg storyboard xib lproj}
     Dir.glob("#{resources_path}/**/*.{#{resource_exts.join(",")}}") do |file|
-      file = file.gsub(/^#{resources_path}\//, '')
       if file.end_with? ".xcdatamodeld"
-        obj = resources.groups.new('isa' => 'XCVersionGroup', 'path' => file, 'sourceTree' => '<group>', 'versionGroupType' => 'wrapper.xcdatamodel')
-        file = obj.files.new('path' => file.gsub(/xcdatamodeld$/, 'xcdatamodel'), 'sourceTree' => '<group>', 'lastKnownFileType' => 'wrapper.xcdatamodel')
-        obj.attributes['currentVersion'] = file.uuid
+        resources.new_xcdatamodel_group(file)
       else
-        resources.files.new('path' => file, 'sourceTree' => '<group>')
+        resources.new_file(file)
       end
     end
 
     Dir.glob("#{pods_headers_path}/**/*.h") do |file|
-      pods.files.new('path' => file)
+      pods.new_file(file)
     end
 
     # Add Basic System Frameworks
@@ -39,7 +33,6 @@ class IB::Project
     frameworks = ["QuartzCore", "CoreGraphics", "CoreData"]
     frameworks.each do |framework|
       file = project.add_system_framework framework
-      target.frameworks_build_phases.first << file
     end
 
     project.save_as("ib.xcodeproj")
